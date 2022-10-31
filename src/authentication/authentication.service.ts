e287096e327340ebb3dec18438d9bfdb11f70934
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-10-31 16:43:08 +0300

    auth added

---
+++ src/authentication/authentication.service.ts
@@ -0,0 +1,62 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { RegisterDto } from './dtos/register.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async register(registerDto: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+    try {
+      const createdUser = await this.usersService.createUser({
+        ...registerDto,
+        password: hashedPassword,
+      });
+      createdUser.password = undefined;
+      return createdUser;
+    } catch (error) {
+      if (error?.code === PostgresErrorCode.UniqueViolation) {
+        throw new HttpException(
+          'User with that email already exists',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      throw new HttpException(
+        'Something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  public async getAuthenticatedUser(email: string, plainTextPassword: string) {
+    try {
+      const user = await this.usersService.getByEmail(email);
+      await this.verifyPassword(plainTextPassword, user.password);
+      user.password = undefined;
+      return user;
+    } catch (error) {
+      throw new HttpException(
+        'Wrong credentials provided',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  private async verifyPassword(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ) {
+    const isPasswordMatching = await bcrypt.compare(
+      plainTextPassword,
+      hashedPassword,
+    );
+    if (!isPasswordMatching) {
+      throw new HttpException(
+        'Wrong credentials provided',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async getByEmail(email: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (!user) throw new HttpException('User not found!', HttpStatus.NOT_FOUND);
+    return user;
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    const newUser = await this.userRepository.create(createUserDto);
+    await this.userRepository.save(newUser);
+    return newUser;
+  }
+}
